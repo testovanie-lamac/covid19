d8248245170c354bc92d292df817a8767d536cbb
Dávid Kováč <qavidko@gmail.com>
2020-10-31 22:24:05 +0100

    fixed waiting count

--- main.js
+++ main.js
@@ -125,6 +125,7 @@ function fetchLamac() {
                         const waitingTime = place.waitingTime.time
                         const waitingCount = place.waitingCount.count;
                         console.log(`Adding ${place.name} - N ${lat} E ${lon} waiting time ${waitingTime}`)
+                        const waitingCountString = (waitingCount < 0) ? '-' : waitingCount
                         const waitingTimeString = (waitingTime < 0) ? 'Neznámy' : `${waitingTime} minút`
 
                         const markerClr = markerColor(waitingCount)
@@ -132,7 +133,7 @@ function fetchLamac() {
                         const waitingCountClr = waitingCountColor(waitingCount)
 
                         const tooltip = `<b>${place.name}: ${location.name}</b><br>` +
-                            `Počet čakajúcich: <b style="color: ${waitingCountClr}">${waitingCount}</b><br>` +// [Aktualizované: ${formatDate(new Date(place.waitingCount.update))}]
+                            `Počet čakajúcich: <b style="color: ${waitingCountClr}">${waitingCountString}</b><br>` +// [Aktualizované: ${formatDate(new Date(place.waitingCount.update))}]
                             `Počet otestovaných: <b>${place.testedCount.count}</b><br>` +// [Aktualizované: ${formatDate(new Date(place.testedCount.update))}]
                             `Odhadovaný čas čakania: <b style="color: ${waitingTimeClr}">${waitingTimeString}</b>` +//[Aktualizované: ${formatDate(new Date(place.waitingTime.update))}]
                             `<br><br>[Aktualizované: ${formatDate(new Date(json.update))}]`//[Aktualizované: ${formatDate(new Date(place.waitingTime.update))}]
